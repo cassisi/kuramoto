f19208a9853aca9cc54626ee778717c574daafa1
Collins Assisi <collins@salk.edu>
2012-04-16 16:21:43 -0700

    Added files kuramoto.m and runKuramoto.m to the repository.

---
+++ kuramoto.m
@@ -0,0 +1,12 @@
+function thetaDot = kuramoto(t,theta,p)
+%equations for a system of weakly coupled kuramoto oscillators
+% inputs, t = time, theta = oscillator phase [nOscillators x 1]
+%         p.w = frequencies [nOscillators x 1], p.K = coupling strength
+%         p.N = number of oscillators
+%outputs, thetadot (input to rk4.m)
+%
+%Kuramoto equations are given by
+%\dot{\theta_{i}} = \omega_{i} + (K/N)*\sum_{j=1}^{j=N} sin(\theta_{j}-\theta_{i})
+
+[theta_j,theta_i] = meshgrid(theta);
+thetaDot = p.w + p.K/p.N*sin(theta_j-theta_i)*ones(p.N,1);
---
+++ runKuramoto.m
@@ -0,0 +1,34 @@
+%Script to integrate kuramoto equations and plot the results
+
+%parameters
+p.K = 3; % coupling strength
+p.N = 100; %number of oscillators
+Omega = 2; %mean frequency
+a = 0; b = Omega+2; %range of oscillator frequencies
+p.w = a + (b-a).*rand(p.N,1); %distribution of osc frequencies
+nIters = 10000;
+
+%initial condition
+thetaInit = -pi + 2*pi.*rand(p.N,1); %uniform distribution \in [-pi,pi]
+
+%integration
+[t,theta] = rk4(@kuramoto,0, 1000,thetaInit,nIters,p);
+
+%plotting
+theta = mod(theta,2*pi); %theta is in the interval [0 2pi]
+theta = theta - pi; %shift the interval to [-pi pi]
+
+for ii = 1:nIters
+    x = cos(theta(:,ii));
+    y = sin(theta(:,ii));
+    mTheta(ii) = circ_mean(theta(:,ii)); %circular mean of theta
+    rTheta(ii) = 1 - circ_std(theta(:,ii)); %circular standard deviation of theta
+    mx = rTheta(ii)*cos(mTheta(ii)); my = rTheta(ii)*sin(mTheta(ii));
+    
+    plot(x,y,'.k'); hold on
+    line([0,mx],[0,my],'Color','r','LineWidth',4); hold off
+    axis([-1 1 -1 1]); axis square
+    M(ii) = getframe;
+end 
+
+movie(M,1)
