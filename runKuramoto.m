af241e41cc4b015b8a14975c48d561cf97122105
Collins Assisi <collins@salk.edu>
2012-04-20 16:39:32 -0700

    Added sorting of theta to runKuramoto.m

--- runKuramoto.m
+++ runKuramoto.m
@@ -1,22 +1,29 @@
-%Script to integrate kuramoto equations and plot the results
+%% Script to integrate kuramoto equations and plot the results
 
 %parameters
-p.K = 3; % coupling strength
-p.N = 5; %number of oscillators
+p.K = 2.5; % coupling strength
+p.N = 25; %number of oscillators
 Omega = 2; %mean frequency
-a = 0; b = Omega+2; %range of oscillator frequencies
+a = 0; b = Omega+5; %range of oscillator frequencies
 p.w = a + (b-a).*rand(p.N,1); %distribution of osc frequencies
-nIters = 1000;
+nIters = 20000;
+tBegin = 0;
+tEnd = 200;
 
 %initial condition
 thetaInit = -pi + 2*pi.*rand(p.N,1); %uniform distribution \in [-pi,pi]
 
 %integration
-[t,theta] = rk4(@kuramoto,0, 100,thetaInit,nIters,p);
+[t,theta] = rk4(@kuramoto,tBegin, tEnd,thetaInit,nIters,p);
 
-%plotting
 theta = mod(theta,2*pi); %theta is in the interval [0 2pi]
 theta = theta - pi; %shift the interval to [-pi pi]
+
+[w,ind] = sort(p.w);
+theta = theta(ind,:);
+
+figure; imagesc(t,[1:p.N],theta)
+%% movie
 
 for ii = 1:nIters
     x = cos(theta(:,ii));
