3464c4bc6dd9413332eac37916fdcf00e0e454b9
Collins Assisi <collins@salk.edu>
2012-04-27 14:22:28 -0700

    added gitignore to ignore *.*~ files modified grapphGenerator.m and graphGeneratorExample.m so that the template martix and the matrix with connections between colors removed are all generated in the same code graphGenerator.m

--- graphGenerators/graphGenerator.m
+++ graphGenerators/graphGenerator.m
@@ -1,13 +1,13 @@
-function G = TemplateGraphGenerator(nPerColor,baseLNLN)
-
+function G = graphGenerator(nPerColor,baseLNLN,baseAdjList,pConn)
+%--------------------------------------------------------------------------
 %function to generate a template network that has the coloring properties
 %of the network specified by baseLNLN
 
 %example of input to the function
-% nPerColor = [10 10 10 10 10 10 10 20];
+% nPerColor = [10 10 10 10 10 10 10 20]; %number of neurons associated with each color
 % 
 % 
-% baseLNLN = [0 1 1 1 1 1 0 0;
+% baseLNLN = [0 1 1 1 1 1 0 0;  % the connectivity between colored groups
 %             1 0 1 1 1 0 1 0;
 %             1 1 0 1 1 1 0 1;
 %             1 1 1 0 1 0 1 0;
@@ -15,6 +15,18 @@ function G = TemplateGraphGenerator(nPerColor,baseLNLN)
 %             1 0 1 0 1 0 0 0;
 %             0 1 0 1 0 0 0 0;
 %             0 0 1 0 0 0 0 0];
+% baseAdjList =  [1,2; %the connections in the baseLNLN adjacency matrix is zero or unity
+%                 2,3; % this list tells you which pairs of groups (colors) do not connect
+%                 3,4; % with each other in an all-to-all manner
+%                 4,5]
+%             
+% pConn = [0.8; %probability of connection between the groups listed in baseAdjList
+%         0.8;
+%         0.8;
+%         0.8];
+
+%--------------------------------------------------------------------------
+
 
 nLN = sum(nPerColor); %number of neurons
 nColors = length(nPerColor); %number of colors
@@ -31,4 +43,17 @@ for ii = 1:size(baseLNLN,2)
     deleteRowsCols = [deleteRowsCols;tmp(1:nDelete(ii),ii)];
 end
 G(deleteRowsCols,:) = [];
-G(:,deleteRowsCols) = [];
+G(:,deleteRowsCols) = [];    
+
+%now run through the probability of connection between different colors
+for ii = 1:size(baseAdjList,1)
+    %determine which block to delete connections from
+    in = baseAdjList(ii,2);
+    out = baseAdjList(ii,1);
+    r = [sum(nPerColor(1:in-1))+1,sum(nPerColor(1:in))]
+    c = [sum(nPerColor(1:out-1))+1,sum(nPerColor(1:out))]
+    
+    rnum = rand(diff(r)+1,diff(c)+1);
+    rnum = double(rnum < pConn(ii));
+    G(r(1):r(2),c(1):c(2)) = rnum.*G(r(1):r(2),c(1):c(2));
+end
--- graphGenerators/graphGeneratorExample.m
+++ graphGenerators/graphGeneratorExample.m
@@ -1,9 +1,9 @@
 %script to generate a digraph from the template
 
 
-nPerColor = [10 10 10 10 10 10 10 10];
+nPerColor = [10 10 10 10 10 10 10 10]; %number of neurons associated with each color
 
-baseLNLN = [0 1 1 1 1 1 0 0;
+baseLNLN = [0 1 1 1 1 1 0 0; % the connectivity between colored groups
             1 0 1 1 1 0 1 0;
             1 1 0 1 1 1 0 1;
             1 1 1 0 1 0 1 0;
@@ -12,47 +12,14 @@ baseLNLN = [0 1 1 1 1 1 0 0;
             0 1 0 1 0 0 0 0;
             0 0 1 0 0 0 0 0];
 
-% baseAdjList =  [1,2;
-%                 2,3;
-%                 3,4;
-%                 4,5]
-% pConn = [0.8;
-%         0.8;
-%         0.8;
-%         0.8];
-    
-baseAdjList = [1,2]
-pConn = 0.8;
-G = TemplateGraphGenerator(nPerColor,baseLNLN);
+baseAdjList =  [1,2; %the connections in the baseLNLN adjacency matrix is zero or unity
+                2,3; % this list tells you which pairs of groups (colors) do not connect
+                3,4; % with each other in an all-to-all manner
+                4,5]
+            
+pConn = [0.8; %probability of connection between the groups listed in baseAdjList
+        0.8;
+        0.8;
+        0.8];
 
-for ii = 1:size(baseAdjList,1)
-    %determine which block to delete connections from
-    in = baseAdjList(ii,2);
-    out = baseAdjList(ii,1);
-    r = [sum(nPerColor(1:in-1))+1,sum(nPerColor(1:in))]
-    c = [sum(nPerColor(1:out-1))+1,sum(nPerColor(1:out))]
-    
-    rnum = rand(diff(r)+1,diff(c)+1);
-    rnum = double(rnum > pConn(ii));
-    G(r(1):r(2),c(1):c(2)) = rnum.*G(r(1):r(2),c(1):c(2));
-end
-
-%printing to file
-nLN = sum(nPerColor);
-nPN = 1;
-LNLN = G;
-LNPN = zeros(nLN,nPN);
-PNLN = zeros(nLN,nPN);
-PNPN = zeros(nPN,nPN);
-PN = zeros(nPN);
-LN = ones(nLN,1);
-
-LNLN = LNLN(:); LNPN = LNPN(:);
-PNPN = PNPN(:); PNLN = PNLN(:);
-
-save C_LNLN.txt LNLN -ascii;
-save C_LNPN.txt LNPN -ascii;
-save C_PNLN.txt PNLN -ascii;
-save C_PNPN.txt PNPN -ascii;
-save C_LN.txt LN -ascii;
-save C_PN.txt PN -ascii;
+G = graphGenerator(nPerColor,baseLNLN,baseAdjList,pConn); %generate the graph
